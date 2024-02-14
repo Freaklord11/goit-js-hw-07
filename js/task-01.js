ec615ff12494f933447c9dcf631cb6ffe2ac1f12
Freaklord11 <141641366+Freaklord11@users.noreply.github.com>
2024-02-14 13:41:42 +0800

    update HW-07

--- js/task-01.js
+++
@@ -1,7 +0,0 @@
-let categories = document.getElementsByClassName("item");
-console.log("Number of Categories: " + categories.length);   
-
-for (let i = 0; i<categories.length; i++){
-    console.log(`Categories: ${categories[i].firstElementChild.innerHTML}`);
-    console.log(`Elements: ${categories[i].firstElementChild.nextElementSibling.children.length}`);
-}
