4d62b82f5ffd77dbf99eefacd3ca1233d0b53b99
Freaklord11 <141641366+Freaklord11@users.noreply.github.com>
2024-02-17 21:29:27 +0800

    task 1 and 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,43 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+galleryItems.forEach((item) => {
+    let li= `
+    <li class="gallery__item">
+        <a class="gallery__link" href="javascript:void(0)">
+            <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+            />
+        </a>
+    </li>
+    `;
+    gallery.innerHTML += li; 
+});
+
+gallery.addEventListener('click', imageClick)
+
+function imageClick(e){
+    if (e.target.tagName === "IMG"){
+        let originalImg = e.target.getAttribute("data-source");
+        const instance = basicLightbox.create(`<img src="${originalImg}" width ="800" height ="600"/>`);
+        instance.show();
+
+        const escapeListener = function(event) {
+            if (event.key === "Escape") {
+                instance.close();
+            }
+        };
+
+        document.addEventListener('keydown', escapeListener);
+
+        // Remove the keyboard listener when the lightbox is closed
+        instance.events.on('close', function() {
+            document.removeEventListener('keydown', escapeListener);
+        });
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('ul.gallery');
+
+//create gallery
+function createGalleryItem(array) {
+    return array.map(({preview, original, description}) =>  {
+        return `
+        <a class = "gallery__item" href ="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>
+        `;
+    }).join(""); 
+}
+
+const picMarkup = createGalleryItem(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", picMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250, animationSpeed: 250});
+lightbox.on("show.simplelightbox");
